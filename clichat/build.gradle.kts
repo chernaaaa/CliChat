6b6ecbf6fe528fa912a069f95258e4477cd93e10
alina chernokulskaia <alina.chernok@gmail.com>
2026-03-30 13:44:00 +0300

    add proto

--- clichat/build.gradle.kts
+++ clichat/build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     id("java")
     checkstyle
+    id("com.google.protobuf") version "0.9.4"
 }
 
-group = "org.example"
+group = "ru.hse.clichat"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -11,9 +12,34 @@ repositories {
 }
 
 dependencies {
+    // gRPC & Protobuf
+    implementation("io.grpc:grpc-netty-shaded:1.62.2")
+    implementation("io.grpc:grpc-protobuf:1.62.2")
+    implementation("io.grpc:grpc-stub:1.62.2")
+    compileOnly("org.apache.tomcat:annotations-api:6.0.53")
+
+    // Тесты
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.25.3"
+    }
+    plugins {
+        create("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.62.2"
+        }
+    }
+    generateProtoTasks {
+        all().forEach {
+            it.plugins {
+                create("grpc")
+            }
+        }
+    }
 }
 
 tasks.test {
